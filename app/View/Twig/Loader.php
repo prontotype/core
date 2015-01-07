8b7e6569349b215f18288cfdbdfedd262cf78547
Mark Perkins <mark@allmarkedup.com>
2015-01-07 15:02:44 +0000

    Fix loader when using namespaces

--- app/View/Twig/Loader.php
+++ app/View/Twig/Loader.php
@@ -19,7 +19,7 @@ class Loader extends \Twig_Loader_Filesystem
             }
             $name = $this->normalizeName($name);
         }
-
+        
         if (isset($this->cache[$name])) {
             return $this->cache[$name];
         }
@@ -27,14 +27,14 @@ class Loader extends \Twig_Loader_Filesystem
         $this->validateName($name);
         
         list($namespace, $shortname) = $this->parseName($name);
-
+        
         if (!isset($this->paths[$namespace])) {
             throw new \Twig_Error_Loader(sprintf('There are no registered paths for namespace "%s".', $namespace));
         }
 
         foreach ($this->paths[$namespace] as $path) {
             $finder = new Finder($path);
-            $results = $finder->guess($name);
+            $results = $finder->guess($shortname);
             if ($results->count()) {
                 return $this->cache[$name] = $results->first();
             }
